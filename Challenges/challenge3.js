594180e0440a822cc30ab16b8b61e964503df584
m11toh <martohmashah@gmail.com>
2024-03-26 00:05:15 +0300

    challenges

--- Challenges/challenge3.js
+++ Challenges/challenge3.js
@@ -1,75 +1,82 @@
-function gross(basicsalary,benefits){
-    const grossAmount = basicsalary + benefits;
-    return grossAmount;
-}
+// deductions
+// nssf
+// nhif
+// payee
+//housing levy
 
-function NHIFdeductions(basicsalary, benefits){
-    const grosssalary = gross(basicsalary, benefits)
-         if(grosssalary >= 0 && grosssalary <= 7999){
-            return 150;
-        }else if(grosssalary >= 8000 && grosssalary <= 11999){
-            return 300;
-        }else if(grosssalary >= 12000 && grosssalary <= 14999){
-            return 400;
-        }else if(grosssalary >= 15000 && grosssalary <= 19999){
-            return 500;
-        }else if(grosssalary >= 20000 && grosssalary <= 24999){
-            return 600;
-        }else if(grosssalary >= 25000 && grosssalary <= 29999){
-            return 750;
-        }else if(grosssalary >= 30000 && grosssalary <= 34000){
-            return 850;
-        }else if(grosssalary >= 35000 && grosssalary <= 39999){
-            return 900;
-        }else if(grosssalary >= 40000 && grosssalary <= 44999){
-            return 950;
-        }else if(grosssalary >= 50000 && grosssalary <= 59999){
-            return 1000;
-        }else{
-            return 1700
-        }
-}
+let grosssalary = 80000
+let benefits = 5000
 
-// calculates income tax
-function tax(basicsalary, benefits){
-    const grosssalary = gross(basicsalary, benefits);
-    const nhif = NHIFdeductions(basicsalary, benefits);
-    const nssfdeductions = calculatenssf(basicsalary, benefits);
-    const taxableincome = grosssalary -(nssfdeductions+nhif);
-    return taxableincome;
+//Grosspay calculations 
+function grosspay(grosssalary,benefits){
 
-}
+     return  grosssalary + benefits ;
+}  
 
-//function of nssf deduction
-function calculatenssf(basicsalary, benefits){
-    const grosssalary = gross(basicsalary, benefits);
-    const nssfdeductions = grosssalary * 0.05;
+//payee calculation as per the KRA
+function calculatepayee(grosspay){
+    console.log(grosspay)
+    if(grosssalary => 24000 && grosssalary <= 288000){
+        payee = grosspay * 0.1
+        return `Your payee is ksh${payee}`;
 
-
-    return nssfdeductions;
-}
-
-//calculates netsalary
-function calculatenetsalary(basicsalary, benefits){
-    const grosssalary = gross(basicsalary, benefits);
-    const nhif = NHIFdeductions(basicsalary, benefits);
-    const nssfdeductions = calculatenssf(basicsalary, benefits);
-    const salary = grosssalary - (nssfdeductions+nhif);
-
-    //conditions
-
-    if(salary  <= 24000){
-        return salary * 0.10;
-    }else if(salary <= 34000){
-        return salary * 0.25;
-    }else if(salary <= 34000){
-        return salary * 0.30;
-    }else if(salary <= 34000){
-        return salary * 0.35;
-    }else{
-        return salary * 0.40;
     }
 }
 
-// net salry after deduction
-console.log(calculatenetsalary(80000, 1000))
+console.log(calculatepayee(grosspay(grosssalary,benefits)))
+
+
+let nssf = 0.06
+function calculateNssf(nssf){
+    nssf = grosssalary * 0.06
+    return `Your nssf is Ksh${nssf}`;
+
+}
+
+console.log(calculateNssf(nssf));
+
+let nhifDeductions = 0
+function calculateNhif(nhifDeductions){
+        if(grosssalary >= 0 && grosssalary <= 5999){
+            return 150;
+        }else if(grosssalary >= 6000 && grosssalary <= 7999){
+            return 300;
+        }else if(grosssalary >= 8000 && grosssalary <= 11999){
+            return 400;
+        }else if(grosssalary >= 12000 && grosssalary <= 14999){
+            return 500;
+        }else if(grosssalary >= 15000 && grosssalary <= 19999){
+            return 600;
+        }else if(grosssalary >= 20000 && grosssalary <= 24999){
+            return 750;
+        }else if(grosssalary >= 25000 && grosssalary <= 29000){
+            return 850;
+        }else if(grosssalary >= 30000 && grosssalary <= 34999){
+            return 900;
+        }else if(grosssalary >= 35000 && grosssalary <= 39999){
+            return 950;
+        }else if(grosssalary >= 40000 && grosssalary <= 44999){
+            return 1000;
+        }else if(grosssalary >= 45000 && grosssalary <= 49999){
+            return 1100;
+        }else if(grosssalary >= 50000 && grosssalary <= 59999){
+            return 1200;
+        }else if(grosssalary >= 60000 && grosssalary <= 69999){
+            return 1300;
+        }else if(grosssalary >= 70000 && grosssalary <= 79999){
+            return 1400;
+        }else if(grosssalary >= 80000 && grosssalary <= 89999){
+            return 1500;
+        }else if(grosssalary >= 90000 && grosssalary <= 99999){
+            return 1600;
+        }
+}
+    console.log(calculateNhif(nhifDeductions));
+
+ function calculatenetsalary(grosssalary){
+    netsalary = grosspay(grosssalary, benefits) - (calculateNhif(nhifDeductions) + (calculateNssf(nhifDeductions)) + (calculatepayee(grosspay)))
+    console.log(calculatenetsalary(netsalary))
+    return netsalary;
+ }
+
+ 
